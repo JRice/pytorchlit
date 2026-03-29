83e347ec488bf5c461f57c8239163deeef4eea48
Jeremy Rice (PC) <jrice@eol.org>
2026-03-29 15:31:49 -0400

    Okay, sloppy, but ... NN calculates loss and backprops

--- more_of.py
+++ more_of.py
@@ -3,6 +3,9 @@ from matplotlib import pyplot as plt
 
 import torch # We need tensors and probability distributions, so we'll use PyTorch for that
 
+import torch.nn.functional as F # For the cross-entropy loss function, which we'll use to evaluate our model's performance
+# It bugs me that we call it "F", but that's convention. Math nerds hate readable names, apparently.
+
 # Following along with Karpathy again: https://www.youtube.com/watch?v=PaCmpygFfXo
 
 words = open('data/input.txt', 'r').read().splitlines()
@@ -46,24 +49,26 @@ def show_heatmap(N, string_to_int, int_to_string):
 
 generator = torch.Generator().manual_seed(2147483647) # Just for reproducibility
 # Note we add one to the counts to avoid zero probabilities, which would cause issues when we take the log of the probabilities later on.
-Probs = (N+1).float() / N.sum(dim=1, keepdim=True) # Convert counts to probabilities
+BigramProbs = (N+1).float() / N.sum(dim=1, keepdim=True) # Convert counts to probabilities
 
 def sample_bigram(N, string_to_int, int_to_string):
     index = 0
     name = ''
 
     while True:
-        prob = Probs[index]
+        prob = BigramProbs[index]
         index = torch.multinomial(prob, num_samples=1, replacement=True, generator=generator).item()
         name += int_to_string[index]
         if index == 0:
             break
     print(f"Generated name: '{name}'")
 
+print("### Simple Sample of Bigrams ###")
+
 for _ in range(10):
     sample_bigram(N, string_to_int, int_to_string)
 
-def evaluate_words(words_list, N, Probs, string_to_int):
+def evaluate_words(words_list, N, BigramProbs, string_to_int):
     negative_log_likelihood = 0.0
     count = 0
     for w in words_list:
@@ -71,7 +76,7 @@ def evaluate_words(words_list, N, Probs, string_to_int):
         for ch1, ch2 in zip(chars, chars[1:]):
             int_ch1 = string_to_int[ch1]
             int_ch2 = string_to_int[ch2]
-            prob = Probs[int_ch1, int_ch2]
+            prob = BigramProbs[int_ch1, int_ch2]
             log_prob = torch.log(prob)
             negative_log_likelihood -= log_prob
             count += 1
@@ -80,6 +85,68 @@ def evaluate_words(words_list, N, Probs, string_to_int):
     print(f"{negative_log_likelihood=:.4f}")
     print(f"Average negative log-likelihood: {negative_log_likelihood / count:.4f}")
     
-evaluate_words(words[:3], N, Probs, string_to_int)
-evaluate_words(['grok'], N, Probs, string_to_int)
-evaluate_words(['jeremyq'], N, Probs, string_to_int)
+evaluate_words(words[:3], N, BigramProbs, string_to_int)
+evaluate_words(['grok'], N, BigramProbs, string_to_int)
+evaluate_words(['jeremyq'], N, BigramProbs, string_to_int)
+
+# Conventionally, x for input, y for output:
+xs, ys = [], []
+
+for w in words[:1]:
+    chars = ['.'] + list(w) + ['.']
+    for ch1, ch2 in zip(chars, chars[1:]):
+        int_ch1 = string_to_int[ch1]
+        int_ch2 = string_to_int[ch2]
+        xs.append(int_ch1)
+        ys.append(int_ch2)
+
+
+def forward_pass(xs, W):
+    xenc = F.one_hot(xs, num_classes=len(string_to_int)).float()
+    logits = xenc @ W # Matrix multiplication to get the logits for the next character
+    counts = logits.exp() # Convert logits to counts by taking the exponential
+    probs = counts / counts.sum(dim=1, keepdim=True) # Convert counts to probabilities
+    print("Probabilities from the untrained model for Emma:")
+    emma_probs = probs[torch.arange(len(words[0]) + 1), ys]
+    print(emma_probs)
+    loss = -emma_probs.log().mean()
+    print('Loss:')
+    print(f'{loss=:.4f}')
+    return loss
+
+def backward_pass(W, loss):
+    W.grad = None
+    loss.backward()
+
+def evaluate_neuron(word, xs, ys, prob):
+    nlls = torch.zeros(len(word))
+    for i in range(len(word)):
+        x = xs[i].item()
+        y = ys[i].item()
+        print("-----")
+        print(f"Bigram example {i+1}: '{int_to_string[x]}{int_to_string[y]}'")
+        print(f"Input: '{int_to_string[x]}' ({x})")
+        print('Output probabilities from the NN:', prob[i])
+        print(f"label: '{int_to_string[y]}' ({y})")
+        p = prob[i, y]
+        print(f"Probability of correct next character '{int_to_string[y]}': {p:.4f}")
+        logp = torch.log(p)
+        print(f"Log-probability of correct next character '{int_to_string[y]}': {logp:.4f}")
+        nll = -logp
+        print(f"Negative log-likelihood: {nll:.4f}")
+        nlls[i] = nll
+    print(f"{nlls.sum()=: .4f}")
+    print(f"Average negative log-likelihood: {nlls.mean().item():.4f}")
+
+# evaluate_neuron(words[0], xs, ys, prob)
+
+xs = torch.tensor(xs)
+ys = torch.tensor(ys)
+
+W = torch.randn(len(string_to_int), len(string_to_int), generator=generator, requires_grad=True) # One-hot encode the input characters
+
+for _ in range(10):
+    loss = forward_pass(xs, W)
+    backward_pass(W, loss)
+    W.data += -0.1 * W.grad # Update the weights using gradient descent
+    loss = forward_pass(xs, W)
